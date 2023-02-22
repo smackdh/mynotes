4068c2c2dbd4b231840519e0217f7e5cc0c4fa3e
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-02-22 15:19:23 +0900

    converted homepage to stateful

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,14 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
