1a4416bd4120c3205a057bb191e837eabd786821
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-02-22 14:48:40 +0900

    added new default homepage

--- lib/main.dart
+++ lib/main.dart
@@ -9,3 +9,12 @@ void main() {
     home: const HomePage(),
   ));
 }
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
