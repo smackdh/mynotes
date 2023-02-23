d56e31070341ea346a5b5298384d0403f5b718f4
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-02-23 13:47:15 +0900

    update scaffold, include firebase init in HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:mynotes/views/login_view.dart';
 
 import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +22,21 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home 🏠")),
-    );
+        appBar: AppBar(
+          title: const Text("Home 🏠"),
+        ),
+        body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                return const Text("Done ✅");
+              default:
+                return const Text('Loading....');
+            }
+          },
+        ));
   }
 }
