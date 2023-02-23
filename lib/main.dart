67ac084497592d9f598e54c348499a683d276c81
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-02-23 14:50:21 +0900

    add verifyemail widget

--- lib/main.dart
+++ lib/main.dart
@@ -48,3 +48,17 @@ class HomePage extends StatelessWidget {
         ));
   }
 }
+
+class VerifyEmailView extends StatefulWidget {
+  const VerifyEmailView({super.key});
+
+  @override
+  State<VerifyEmailView> createState() => _VerifyEmailViewState();
+}
+
+class _VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
