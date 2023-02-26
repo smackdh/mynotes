07cf8e405513e9e6abf2ff5829bed8082f872e44
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-02-27 07:24:00 +0900

    change loading to use indicator

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class HomePage extends StatelessWidget {
             // return const Text("Done ✅");
             return const LoginView();
           default:
-            return const Text('Loading....');
+            return const CircularProgressIndicator();
         }
       },
     );
