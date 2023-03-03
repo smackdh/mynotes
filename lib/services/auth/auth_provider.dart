c67740535f7db1853ba25175fa44513c3751d0fd
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-03-03 15:56:29 +0900

    added future with requirement email passw

--- lib/services/auth/auth_provider.dart
+++ lib/services/auth/auth_provider.dart
@@ -2,4 +2,8 @@ import 'package:mynotes/services/auth/auth_user.dart';
 
 abstract class AuthProvider {
   AuthUser? get currentUser;
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  });
 }
