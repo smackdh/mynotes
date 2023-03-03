2b9e2bb92b148459a62ec9e56824b8fb906cbd83
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-03-03 16:14:47 +0900

    add functions from authprovider

---
+++ lib/services/auth/firebase_auth_provider.dart
@@ -0,0 +1,7 @@
+import 'package:mynotes/services/auth/auth_user.dart';
+import 'package:mynotes/services/auth/auth_provider.dart';
+import 'package:mynotes/services/auth/auth_exceptions.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FireBaseAuthProvider implements AuthProvider {}
