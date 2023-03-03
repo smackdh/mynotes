40871661bfbe7e425c02434f234830febd24e0d3
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-03-03 14:29:46 +0900

    add generic exceptions

--- lib/services/auth/auth_exceptions.dart
+++ lib/services/auth/auth_exceptions.dart
@@ -11,3 +11,9 @@ class WeakPasswordAuthException implements Exception {}
 class EmailAlreadyInUseAuthException implements Exception {}
 
 class InvalidEmailAuthException implements Exception {}
+
+// Generic Exceptions
+
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
