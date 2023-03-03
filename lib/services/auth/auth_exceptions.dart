4621fbf4112f98c5e7ffd120ace7d740842823f9
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-03-03 14:27:16 +0900

    add register auth exceptions

--- lib/services/auth/auth_exceptions.dart
+++ lib/services/auth/auth_exceptions.dart
@@ -1,3 +1,13 @@
+// Login Exceptions
+
 class UserNotFoundAuthException implements Exception {}
 
 class WrongPasswordAuthException implements Exception {}
+
+// Register Exceptions
+
+class WeakPasswordAuthException implements Exception {}
+
+class EmailAlreadyInUseAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
