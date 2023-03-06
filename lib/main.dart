8c7690abed459e30b07d853c3bfa58419610b5b3
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-03-06 09:55:42 +0900

    moved notesview to separate file and imported required packages

--- lib/main.dart
+++ lib/main.dart
@@ -59,48 +59,6 @@ class HomePage extends StatelessWidget {
 
 enum MenuAction { logout }
 
-class NotesView extends StatefulWidget {
-  const NotesView({super.key});
-
-  @override
-  State<NotesView> createState() => _NotesViewState();
-}
-
-class _NotesViewState extends State<NotesView> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Notes 📝'),
-        
-        actions: [
-          PopupMenuButton<MenuAction>(
-            onSelected: (value) async {
-              switch (value) {
-                case MenuAction.logout:
-                  final shouldLogout = await showLogOutDialog(context);
-                  if (shouldLogout) {
-                    await FirebaseAuth.instance.signOut();
-                    Navigator.of(context)
-                        .pushNamedAndRemoveUntil(loginRoute, (_) => false);
-                  }
-              }
-            },
-            itemBuilder: (context) {
-              return const [
-                PopupMenuItem<MenuAction>(
-                    value: MenuAction.logout, child: Text('Logout')),
-                PopupMenuItem<MenuAction>(child: Text('Second Page'))
-              ];
-            },
-          )
-        ],
-      ),
-      body: const Text('Hello NotesView'),
-    );
-  }
-}
-
 Future<bool> showLogOutDialog(BuildContext context) {
   return showDialog<bool>(
     context: context,
---
+++ lib/views/notes_view.dart
@@ -0,0 +1,47 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../main.dart';
+import 'constants/routes.dart';
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Notes 📝'),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogOutDialog(context);
+                  if (shouldLogout) {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil(loginRoute, (_) => false);
+                  }
+              }
+            },
+            itemBuilder: (context) {
+              return const [
+                PopupMenuItem<MenuAction>(
+                    value: MenuAction.logout, child: Text('Logout')),
+                PopupMenuItem<MenuAction>(child: Text('Second Page'))
+              ];
+            },
+          )
+        ],
+      ),
+      body: const Text('Hello NotesView'),
+    );
+  }
+}
